a12602eac4834461b6ce8f20c53af9394dc6cf72
Arda Güçlü <aguclu@redhat.com>
2025-01-27 10:33:57 +0300

    Implement operator client and wire through cmd

---
+++ pkg/cmd/operator/cmd.go
@@ -0,0 +1,22 @@
+package operator
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+	"k8s.io/utils/clock"
+
+	"github.com/openshift/library-go/pkg/controller/controllercmd"
+	"github.com/openshift/lws-operator/pkg/operator"
+	"github.com/openshift/lws-operator/pkg/version"
+)
+
+func NewOperator(ctx context.Context) *cobra.Command {
+	cmd := controllercmd.
+		NewControllerCommandConfig("openshift-lws-operator", version.Get(), operator.RunOperator, clock.RealClock{}).
+		NewCommandWithContext(ctx)
+	cmd.Use = "operator"
+	cmd.Short = "Start the Cluster LWS Operator"
+
+	return cmd
+}
