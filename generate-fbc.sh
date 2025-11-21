29e2631fbd09eb40fb8df513736c167b7b6e6b96
Kevin Hannon <kehannon@redhat.com>
2025-11-21 16:10:27 -0500

    add v4.21 to lws

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -1,3 +1,3 @@
-for OCP_VERSION in v4.18 v4.19 v4.20; do
+for OCP_VERSION in v4.18 v4.19 v4.20 v4.21; do
     opm alpha render-template semver $OCP_VERSION/catalog-template.yaml --migrate-level=bundle-object-to-csv-metadata > $OCP_VERSION/catalog/lws-operator/catalog.json;
 done
